c7d526859d46550b9914589dd119c766fb5e51bc
Fred Denis <fred.denis3@gmail.com>
2021-07-24 23:41:00 +1000

    dev

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+# Fred Denis -- 
+#
+set -o pipefail
+#
+     TS="date "+%Y-%m-%d_%H%M%S""          # A timestamp for a nice output in a logfile
  TMPDIR="/tmp"
     dir=$(mktemp -du fastestfindXXXXXXXXX -p ${TMPDIR})
 nbtests=4
@@ -9,13 +13,18 @@ FIND1="-delete"
 FIND2="-exec rm -fr {} \;"
 FIND3="| xargs -0 rm -fr"
 
-#COMMAND1="time find ${dir} -type f -delete"
-#COMMAND2="time find ${dir} -type f -exec rm -fr {} \;"
-#COMMAND3="time find ${dir} -type f | xargs -0 rm -fr"
-
 for i in $(seq 1 ${nbtests}); do
-#    echo $i
-    ./cre_files --dir ${dir} --nb ${nbfiles} > dev/null
-    [[ -f ${dir} ]] && find ${dir} -type f ${FIND1}  #> /dev/null
+    if [[ -f ./cre_files.sh ]]; then
+        ./cre_files.sh --dir "${dir}" --nb ${nbfiles} > dev/null
+    else
+        printf "\033[1;31m%s\033[m\n" "$($TS) [ERROR] Cannot find cre_files.sh"
+        exit 123
+    fi
+    if [[ -d "${dir}" ]]; then
+        find "${dir}" -type f ${FIND1}  > /dev/null
+    else
+        printf "\033[1;31m%s\033[m\n" "$($TS) [ERROR] ${dir} does not exist, cannot continue."
+        exit 124
+    fi
     rm -fr "${dir}"
 done
