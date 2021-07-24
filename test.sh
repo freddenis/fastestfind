e3c876b1e1834a4a716b79749caa49c0cdd3d6f2
Fred Denis <fred.denis3@gmail.com>
2021-07-24 23:31:18 +1000

    better test

--- test.sh
+++ test.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-dir="tmp"
+ TMPDIR="/tmp"
+    dir=$(mktemp -du fastestfindXXXXXXXXX -p ${TMPDIR})
+nbtests=4
+nbfiles=1000
 
-COMMAND1="time find $dir -type f -delete"
-COMMAND2="time find $dir -type f -exec rm -fr {} \;"
-COMMAND3="time find $dir -type f | xargs -0 rm -fr"
+FIND1="-delete"
+FIND2="-exec rm -fr {} \;"
+FIND3="| xargs -0 rm -fr"
+
+#COMMAND1="time find ${dir} -type f -delete"
+#COMMAND2="time find ${dir} -type f -exec rm -fr {} \;"
+#COMMAND3="time find ${dir} -type f | xargs -0 rm -fr"
+
+for i in $(seq 1 ${nbtests}); do
+#    echo $i
+    ./cre_files --dir ${dir} --nb ${nbfiles} > dev/null
+    [[ -f ${dir} ]] && find ${dir} -type f ${FIND1}  #> /dev/null
+    rm -fr "${dir}"
+done
