2ea9ad10c4d636f13a53f7db0587259ca7f98c95
Fred Denis <fred.denis3@gmail.com>
2021-07-25 00:26:49 +1000

    dev

--- test.sh
+++ test.sh
@@ -7,21 +7,29 @@ set -o pipefail
  TMPDIR="/tmp"
     dir=$(mktemp -du fastestfindXXXXXXXXX -p ${TMPDIR})
 nbtests=4
-nbfiles=1000
-
+nbfiles=100000
+#
 FIND1="-delete"
 FIND2="-exec rm -fr {} \;"
 FIND3="| xargs -0 rm -fr"
-
+#
 for i in $(seq 1 ${nbtests}); do
     if [[ -f ./cre_files.sh ]]; then
-        ./cre_files.sh --dir "${dir}" --nb ${nbfiles} > dev/null
+        start=$(date +%s)
+        ./cre_files.sh --dir "${dir}" --nb ${nbfiles} > /dev/null
+        end=$(date +%s)
+        seconds=$(( end - start ))
+        printf "\033[1;36m%s\033[m\n" "$($TS) [INFO] create ${nbfiles} files in ${seconds} seconds"
     else
         printf "\033[1;31m%s\033[m\n" "$($TS) [ERROR] Cannot find cre_files.sh"
         exit 123
     fi
     if [[ -d "${dir}" ]]; then
+        start=$(date +%s)
         find "${dir}" -type f ${FIND1}  > /dev/null
+        end=$(date +%s)
+        seconds=$(( end - start ))
+        printf "\033[1;36m%s\033[m\n" "$($TS) [INFO] ${FIND1} in ${seconds} seconds"
     else
         printf "\033[1;31m%s\033[m\n" "$($TS) [ERROR] ${dir} does not exist, cannot continue."
         exit 124
