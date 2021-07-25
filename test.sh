f703f1180933814b04e1e17816b6f103c41c1cd1
Fred Denis <fred.denis3@gmail.com>
2021-07-25 21:38:09 +1000

    dev

--- test.sh
+++ test.sh
@@ -6,17 +6,26 @@ set -o pipefail
      TS="date "+%Y-%m-%d_%H%M%S""          # A timestamp for a nice output in a logfile
  TMPDIR="/tmp"
     dir=$(mktemp -du fastestfindXXXXXXXXX -p ${TMPDIR})
-nbtests=3
+nbtests=5
 nbfiles=100000
 #
-FIND1="-delete"
-FIND2="-exec rm -fr {} \;"
-FIND3="| xargs -0 rm -fr"
-
-for WHAT in FIND1 FIND2 FIND3; do
-    echo ${!WHAT}
-    printf "\033[1;36m%s\033[m\n" "Fastestfind test with ${nbfiles}: ${!WHAT}"
+FIND="find ${dir} -type f "
+OPT1="-delete"
+OPT2="-exec rm -f {} \;"
+OPT3="| xargs -0 -n 10 rm -f"
+OPT3="| xargs rm -f"
+#
+#for WHAT in OPT1 OPT2 OPT3; do
+for WHAT in OPT3; do
+#    echo ${!WHAT}
+    echo ${FIND}${!WHAT}
+    printf "\033[1;36m%s\033[m\n" "********************************************************************************************"
+    printf "\033[1;36m%s\033[m\n" "Fastestfind test with ${nbfiles} files and file deletion with find option: ${!WHAT}"
+    printf "\033[1;36m%s\033[m\n" "********************************************************************************************"
     for i in $(seq 1 ${nbtests}); do
+        #
+        # Create the files
+        #
         if [[ -f ./cre_files.sh ]]; then
             start=$(date +%s)
             printf "\033[1;36m%s\033[m" "$($TS) [INFO] Creating ${nbfiles} in ${dir} . . ."
@@ -28,12 +37,16 @@ for WHAT in FIND1 FIND2 FIND3; do
             printf "\033[1;31m%s\033[m\n" "$($TS) [ERROR] Cannot find cre_files.sh"
             exit 123
         fi
-        if [[ -d "${dir}" ]]; then
+        #
+        # Delete the files
+        #
+        if [[ -d "${dir}"  || -n "${ECHO}" ]]; then
             start=$(date +%s)
-            find "${dir}" -type f ${FIND1}  > /dev/null
+            echo ${FIND}${!WHAT}
+            eval ${FIND}${!WHAT}  #> /dev/null
             end=$(date +%s)
             seconds=$(( end - start ))
-            printf "\033[1;36m%s\033[m\n" "$($TS) [INFO] ${FIND1} in ${seconds} seconds"
+            printf "\033[1;36m%s\033[m\n" "$($TS) [INFO] ${!WHAT} in ${seconds} seconds"
         else
             printf "\033[1;31m%s\033[m\n" "$($TS) [ERROR] ${dir} does not exist, cannot continue."
             exit 124
